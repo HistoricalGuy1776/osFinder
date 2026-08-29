cb7243820334d4d11268c00717ef585739dc6d8e
HistoryGuy777 <barrett5.0hp@gmail.com>
2026-08-28 23:28:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/osfinder/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.osfinder;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        int Windows = 0;
+        int Linux = 0;
+        int MacOS = 0;
+        int iOS = 0;
+        int AndroidOS = 0;
+        int ChromeOS = 0;
+        int SteamOS = 0;
+        int highestValue = 0;
+        System.out.println("How much do you value security?");
+    }
+}
